b08ce39a72b4e614829ae7e313d900c3ea634367
jogex93 <jogex123@gmail.com>
2016-10-28 19:22:20 +0200

    Nye filer

---
+++ js/userInteract.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Jørgen on 28.10.2016.
+ */
+$(document).ready(function () {
+    $.ajax({
+
+    });
+
+    $("body").on('click', function () {
+        // $("#meny").css("bottom", "0");
+        $("#meny").animate({bottom: "0"}, 500);
+    });
+
+    $("#menyClose").on('click', function() {
+
+    });
+});
