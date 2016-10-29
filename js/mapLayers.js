2c59402664be5db91992df8a6c387a2cf64c2a5e
MagnusPoppe <Magnus.Poppe.Wang@gmail.com>
2016-10-29 02:17:13 +0200

    Hotspots bruker naa live data fra Norges Reiseinformasjon

--- js/mapLayers.js
+++ js/mapLayers.js
@@ -24,7 +24,7 @@ function initializeMap( position )
     	var marker = L.marker(e.latlng).addTo(map);
     	marker.bindPopup("koordinater: "+e.latlng);
 	}
-	map.on('click', onMapClick);
+	//map.on('click', onMapClick);
 }
 
 function setRoadLayer() 
@@ -67,9 +67,18 @@ function getDepartureTimes( bus_stop_id )
 function setBusStops( pos )
 {
 	var stops = getBusstops( pos[0], pos[1] );
-
+	console.log(stops[0]);
 	for(var i = 0; i < stops.length; i++)
-		placeIcon(AVAILABLE_HOTSPOT_ICON,  stops[i].latitude, stops[i].longitude );
+	{
+		var icon = placeIcon(ACTIVE_HOTSPOT_ICON,  stops[i].latitude, stops[i].longitude );
+		var id = stops[i].id;
+
+		icon.on("click", function(e) {
+			plannerInfoFill( id );
+			$("#meny").animate({bottom: "0"}, 500);
+
+		});
+	}
 }
 
 function getBusstops( latitude, longitude )
